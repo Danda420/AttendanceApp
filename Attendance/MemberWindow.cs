eaf3e9fd11e44ad46ac2de3b5e448c0ee76ac5ca
Danda <dandagaharu77@gmail.com>
2023-12-14 17:05:33 +0700

    MemberWindow: show current user email, name & attendance. And absennnnnnn

--- Attendance/MemberWindow.cs
+++ Attendance/MemberWindow.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,19 +8,96 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace Attendance
 {
     public partial class MemberWindow : Form
     {
+        public MySqlConnection conn = new MySqlConnection("server=127.0.0.1;user=root;database=attendance;password=");
+        MySqlCommand cmd = new MySqlCommand();
+        private MySqlDataAdapter adapter;
+        private DataTable dataTable;
+
+        string loggedInEmail = LoginForm.Session.loggedInEmail;
+        string loggedInName = LoginForm.Session.loggedInName;
+
         public MemberWindow()
         {
             InitializeComponent();
         }
 
+        public void updateTable()
+        {
+            conn.Open();
+            string query = $"SELECT * FROM attendance WHERE nama = '{loggedInName}'";
+            cmd = new MySqlCommand(query, conn);
+
+            adapter = new MySqlDataAdapter(cmd);
+            dataTable = new DataTable();
+
+            adapter.Fill(dataTable);
+
+            dataGridView1.DataSource = dataTable;
+
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                if (column.HeaderText == "id")
+                {
+                    column.Visible = false;
+                }
+                else
+                {
+                    column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                }
+            }
+            conn.Close();
+        }
+
         private void Member_Load(object sender, EventArgs e)
         {
-            
+            Email.Text = $"Email : {loggedInEmail}";
+            lblNama.Text = $"Nama : {loggedInName}";
+            updateTable();
+        }
+
+        private void btnAbsen_Click(object sender, EventArgs e)
+        {
+            string status = null;
+
+            if (rbHadir.Checked)
+            {
+                status = "Hadir";
+            }
+            else if (rbIzin.Checked)
+            {
+                status = "Izin";
+            }
+            else if (rbAlfa.Checked)
+            {
+                status = "Alfa";
+            }
+
+            DateTime currentDate = DateTime.Now;
+            string date = currentDate.ToString("yyyy-MM-dd");
+
+            conn.Open();
+            string absen = $"INSERT INTO attendance (nama, date, attendance) VALUES ('{loggedInName}', '{date}', '{status}')";
+            cmd = new MySqlCommand(absen, conn);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            updateTable();
+        }
+
+        private void btnLogout_Click(object sender, EventArgs e)
+        {
+            LoginForm.Session.destroySession();
+
+            LoginForm loginWindowInstance = new LoginForm();
+            loginWindowInstance.Show();
+
+            this.Close();
         }
     }
 }
