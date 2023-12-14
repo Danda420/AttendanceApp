ccb934647b24f47d05757ccdcf296abb29259fc5
Danda <dandagaharu77@gmail.com>
2023-12-14 17:01:33 +0700

    database.sql: update users table and add attendance table

--- database.sql
+++ database.sql
@@ -4,8 +4,17 @@ USE attendance;
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL,
+  `pangkat` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `attendance` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) NOT NULL,
+  `date` date NOT NULL,
+  `attendance` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 );
