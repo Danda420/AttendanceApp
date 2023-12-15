62ad0a4852b7d358a2bb4fd2a5d4e6638ee80191
Danda <dandagaharu77@gmail.com>
2023-12-15 20:30:13 +0700

    database.sql: update attendance table and insert admeme account immediately

--- database.sql
+++ database.sql
@@ -14,7 +14,10 @@ CREATE TABLE `users` (
 CREATE TABLE `attendance` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nama` varchar(255) NOT NULL,
-  `date` date NOT NULL,
+  `date` date NOT NULL DEFAULT CURRENT_DATE,
+  `time` time NOT NULL DEFAULT CURRENT_TIME,
   `attendance` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 );
+
+INSERT INTO users (email, nama, pangkat, password) VALUES ('admin@gmail.com', 'danda', 'admin', 'admin')
